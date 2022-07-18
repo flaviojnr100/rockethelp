31267be9aa1e1461252eb4f9885d9659cac32042
flaviojnr100 <flaviojunior1009@gmail.com>
2022-07-18 15:38:27 -0300

    Criação da tela de login

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import {VStack, Heading,Icon, useTheme } from 'native-base'
+import { Envelope, Key} from 'phosphor-react-native';
+import Logo from '../assets/logo_primary.svg';
+
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
+export function SignIn(){
+    const {colors} = useTheme();
+    const [name,setName] = useState('');
+    const [password,setPassword] = useState('');
+
+    function handleSignIn(){
+        console.log({name,password});
+    }
+    
+    return(
+        <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+            <Logo/>
+            <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+                Acesse sua conta
+            </Heading>
+
+            <Input placeholder="E-mail" mb={4} InputLeftElement={<Icon as={<Envelope color={colors.gray[300]}/>} ml={6}/>} onChangeText={text=>setName(text)}/>
+            <Input mb={8} placeholder="Senha" InputLeftElement={<Icon as={<Key color={colors.gray[300]}/>} ml={6}/>} secureTextEntry onChangeText={text=>setPassword(text)}/>
+
+            <Button title="Entrar" w="full" onPress={handleSignIn}/>
+        </VStack>
+    )
+}
